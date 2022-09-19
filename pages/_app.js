20413afe03d0e3df5f1975bb325057b67517be99
schaldach <gabimorgado0311@gmail.com>
2022-09-19 16:04:09 -0300

    key nas barras

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import '../styles/index.css'
 import NavBar from '../components/Navbar'
+import Head from 'next/head'
 import React, {useState} from 'react'
 
 function MyApp({ Component, pageProps }) {
   const[darkMode, setDark] = useState(false)
   return (
   <>
+  <Head>
+    <title>Algoritmos</title>
+  </Head>
   <div className='pagelayout' data-theme={darkMode?'dark':'light'}>
     <NavBar/>
     <main>
--- pages/sorting/index.js
+++ pages/sorting/index.js
@@ -1,6 +1,24 @@
+import React, {useState} from "react";
+
 function Sorting() {
+    const [currentAlgo, changeAlgo] = useState('binarysort')
+    const [sortedArray, changeArray] = useState([1,2,3,4,5,6,7,8,9,10])
+
+    function returnString(){
+        let newString = ''
+        for(let i=0; i<sortedArray.length; i++){
+            newString+='1fr '
+        }
+        return newString
+    }
+    
     return (
-        <div></div>
+        <div className="Sortingalgorithm">
+            <div className="Sortingtitle"></div>
+            <div className="Sortingbars" style={{gridTemplateColumns:returnString()}}>
+                {sortedArray.map((counter,index) => <div key={index}><div style={{height:String(counter*100/sortedArray.length)+'%'}}/></div>)}
+            </div>
+        </div>
     );
 }
 
