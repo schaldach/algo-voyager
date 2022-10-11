a092d1fbd3b8c833364c60045aac1728f49870c4
schaldach <gabimorgado0311@gmail.com>
2022-10-11 14:09:39 -0300

    mexendo nos parametros da perlin

--- pages/noise.js
+++ pages/noise.js
@@ -11,9 +11,9 @@ function Noise() {
 
     useEffect(() => {
         let newMap = []
-        for(let i=0; i<75; i++){
+        for(let i=0; i<125; i++){
             let row = []
-            for(let y=0; y<225; y++){
+            for(let y=0; y<375; y++){
                 row.push({noise:0})
             }
             newMap.push(row)
@@ -31,14 +31,14 @@ function Noise() {
             for(let i=0; i<map2D.length; i++){
                 for(let j=0; j<map2D[i].length-1; j++){
                     if(map2D[i][j].noise !== map2D[i][j+1].noise){
-                        context.fillRect(j*5+5, i*5, 1, 5)
+                        context.fillRect(j*3+3, i*3, 1, 3)
                     }
                 }
             }
             for(let i2=0; i2<map2D.length-1; i2++){
                 for(let j2=0; j2<map2D[i2].length; j2++){
                     if(map2D[i2][j2].noise !== map2D[i2+1][j2].noise){
-                        context.fillRect(j2*5, i2*5+5, 5, 1)
+                        context.fillRect(j2*3, i2*3+3, 3, 1)
                     }
                 }
             }
@@ -47,7 +47,7 @@ function Noise() {
             for(let y=0; y<map2D.length; y++){
                 for(let x=0; x<map2D[y].length; x++){
                     context.fillStyle = staticColors[map2D[y][x].noise]
-                    context.fillRect(x*5, y*5, 5, 5)
+                    context.fillRect(x*3, y*3, 3, 3)
                 }
             }
         }
@@ -72,9 +72,9 @@ function Noise() {
 
     function randomNoise(){
         let newMap = []
-        for(let i=0; i<75; i++){
+        for(let i=0; i<125; i++){
             let row = []
-            for(let y=0; y<225; y++){
+            for(let y=0; y<375; y++){
                 row.push({noise:0})
             }
             newMap.push(row)
@@ -90,9 +90,9 @@ function Noise() {
 
     function perlinNoise2D(){
         let newMap = []
-        for(let i=0; i<75; i++){
+        for(let i=0; i<125; i++){
             let row = []
-            for(let y=0; y<225; y++){
+            for(let y=0; y<375; y++){
                 row.push({noise:0})
             }
             newMap.push(row)
@@ -100,7 +100,7 @@ function Noise() {
         newMap.forEach((row,y) => {
             row.forEach((cell,x) => {
                 // converter range do perlin noise {-sqrt(2)/2, sqrt(2)/2} para {-1,1}
-                let noiseValue = perlin.get((x+0.5)/8, (y+0.5)/8)*1.41
+                let noiseValue = perlin.get((x+0.5)/12, (y+0.5)/12)*1.41
                 let actualValue = noiseValue
                 if(noiseValue<1){actualValue = 4}
                 if(noiseValue<0.6){actualValue = 3}
@@ -120,7 +120,7 @@ function Noise() {
             newLine.push({height:0})
         }
         newLine.forEach((vertex, x) => {
-            let noiseValue = perlin.get((x+0.5)/20, 0.5/20)*1.41
+            let noiseValue = perlin.get((x+0.5)/15, 0.5/15)*1.41
             let yPosition = (noiseValue+1)*375/2
             newLine[x].height = yPosition
         })
