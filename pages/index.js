fb6689ff770d392cac3dd7cbac65af7119c83588
schaldach <gabimorgado0311@gmail.com>
2024-01-12 19:21:38 -0300

    Nome definitivo

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="homescreen">
-      <div>AlgoExplorer</div>
+      <div>AlgoVoyager</div>
       <div>Uma ferramenta para a visualização de diferentes algoritmos empregados na Ciência da Computação.</div>
       <div>Desenvolvido por Schaldach.</div>
       <div>github</div>
