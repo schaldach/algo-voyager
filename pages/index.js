24f4e74b5f44ae796a6907d42836f0e6489d790f
schaldach <gabimorgado0311@gmail.com>
2024-01-12 19:19:56 -0300

    Novo nome

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-export default function Home({changePage}) {
+export default function Home() {
   return (
     <div className="homescreen">
-      <div></div>
+      <div>AlgoExplorer</div>
       <div>Uma ferramenta para a visualização de diferentes algoritmos empregados na Ciência da Computação.</div>
       <div>Desenvolvido por Schaldach.</div>
+      <div>github</div>
     </div>
   )
 }
